b4f7af8b6b26565af8f716c2146a9b7797522628
Mayank <mayank@gmail.com>
2025-08-13 13:46:30 +0530

    testing

---
+++ test.py
@@ -0,0 +1,14 @@
+import logging
+
+logger = logging.getLogger('my_app')
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
+logger.info("This is a custom logger message")
